0c9d54a2ab8188908b9093778114cba7bcdef7ff
Bernardo Barzotto Zomer <bernardobarzottoz@gmail.com>
2022-10-12 02:36:35 -0300

    merge(#1): master <- wip-base
    Add basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,77 @@
-static mut LEADER_ID: usize = 0;
+use anyhow::Result;
+use crossbeam::channel::{bounded, Receiver, Sender};
+use crossbeam::thread;
+
+const RING_SIZE: usize = 3;
 
 fn main() {
-    let mut ring = [
-        Actor::new(0),
-        Actor::new(1),
-        Actor::new(2),
-        Actor::new(3),
-    ];
+    // Create a channel for each ring member.
+    let chans: [(Sender<Msg>, Receiver<Msg>); RING_SIZE] = (0..RING_SIZE)
+        .map(|_| {
+            bounded(1)
+        })
+        .collect::<Vec<_>>()
+        .try_into()
+        .unwrap();
 
-    loop {
-        unimplemented!();
-    }
+    // Create a channel for the controller.
+    let (ctrl_s, ctrl_r) = bounded(1);
+
+    // Spawn a thread for each ring member and one for the controller.
+    // Each ring member receives on its channel and sends on the next's.
+    thread::scope(|scope| {
+        for i in 0..RING_SIZE {
+            let (s, r) = (
+                match chans.get(i + 1) {
+                    Some(next) => next.0.clone(),
+                    None => chans[0].0.clone()
+                },
+                chans[i].1.clone()
+            );
+
+            let ctrl_s = ctrl_s.clone();
+            scope.spawn(move |_| election_stage(i as i32, s, r, ctrl_s));
+        }
+
+        println!("main: election ring created");
+        let (first_s, ctrl_r) = (chans[0].0.clone(), ctrl_r.clone());
+        scope.spawn(move |_| election_controller(first_s, ctrl_r));
+    }).unwrap();
+
+    println!("main: done");
 }
 
-#[derive(Debug)]
-struct Actor {
-    id: usize,
-    active: bool,
+fn election_controller(
+    s: Sender<Msg>, ctrl_r: Receiver<u8>
+) -> Result<()> {
+    s.send(Msg { body: [-1; RING_SIZE] })?;
+    println!("ctrl: election started");
+    ctrl_r.recv()?;
+    println!("ctrl: election ended");
+    Ok(())
 }
 
-impl Actor {
-    pub fn new(id: usize) -> Self {
-        Self { id, active: true }
+fn election_stage(
+    id: i32, s: Sender<Msg>, r: Receiver<Msg>, ctrl_s: Sender<u8>
+) -> Result<()> {
+    let mut msg = r.recv()?;
+    println!("{}: received message {:?}", id, msg);
+    msg.body[id as usize] = id;
+    s.send(msg)?;
+    println!("{}: sent message", id);
+
+    if id == 0 {
+        let msg = r.recv()?;
+        println!("{}: received message {:?}", id, msg);
+        ctrl_s.send(0)?;
+        println!("{}: sent confirmation to ctrl", id);
     }
+
+    println!("{}: done", id);
+    Ok(())
 }
 
 #[derive(Debug)]
 struct Msg {
-    kind: i32,
-    body: [i32; 3]
+    body: [i32; RING_SIZE]
 }
