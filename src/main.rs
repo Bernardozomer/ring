57fa9ef5f0f64d28112b866e9d10980015adead8
Bernardozomer <bernardobarzottoz@gmail.com>
2022-10-14 01:20:35 -0300

    refactor: rename controller to simulator

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,8 @@ fn main() {
         .try_into()
         .unwrap();
 
-    // Create a channel for the controller.
-    let (ctrl_s, ctrl_r) = bounded(1);
+    // Create a channel for the simulator.
+    let (sim_s, sim_r) = bounded(1);
 
     // Spawn a thread for each ring member and one for the controller.
     // Each ring member receives on its channel and sends on the next's.
@@ -29,30 +29,30 @@ fn main() {
                 chans[i].1.clone()
             );
 
-            let ctrl_s = ctrl_s.clone();
-            scope.spawn(move |_| election_stage(i as i32, s, r, ctrl_s));
+            let sim_s = sim_s.clone();
+            scope.spawn(move |_| election_stage(i as i32, s, r, sim_s));
         }
 
         println!("main: election ring created");
-        let (first_s, ctrl_r) = (chans[0].0.clone(), ctrl_r.clone());
-        scope.spawn(move |_| election_controller(first_s, ctrl_r));
+        let (first_s, sim_r) = (chans[0].0.clone(), sim_r.clone());
+        scope.spawn(move |_| sim_election(first_s, sim_r));
     }).unwrap();
 
     println!("main: done");
 }
 
-fn election_controller(
-    s: Sender<Msg>, ctrl_r: Receiver<u8>
+fn sim_election(
+    s: Sender<Msg>, sim_r: Receiver<u8>
 ) -> Result<()> {
     s.send(Msg { body: [-1; RING_SIZE] })?;
-    println!("ctrl: election started");
-    ctrl_r.recv()?;
-    println!("ctrl: election ended");
+    println!("sim: election started");
+    sim_r.recv()?;
+    println!("sim: election ended");
     Ok(())
 }
 
 fn election_stage(
-    id: i32, s: Sender<Msg>, r: Receiver<Msg>, ctrl_s: Sender<u8>
+    id: i32, s: Sender<Msg>, r: Receiver<Msg>, sim_s: Sender<u8>
 ) -> Result<()> {
     let mut msg = r.recv()?;
     println!("{}: received message {:?}", id, msg);
@@ -63,8 +63,8 @@ fn election_stage(
     if id == 0 {
         let msg = r.recv()?;
         println!("{}: received message {:?}", id, msg);
-        ctrl_s.send(0)?;
-        println!("{}: sent confirmation to ctrl", id);
+        sim_s.send(0)?;
+        println!("{}: sent confirmation to sim", id);
     }
 
     println!("{}: done", id);
